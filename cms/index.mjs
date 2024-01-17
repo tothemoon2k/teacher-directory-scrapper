dc2b40fe1d9dbe208e8b1ca89ac29ad8a3a45286
Justin Garner <charlottewebsolutions1@gmail.com>
2024-01-17 11:14:14 -0500

    Scrapper runs smoothly and handles errors on page like a champ

--- cms/index.mjs
+++ cms/index.mjs
@@ -8,31 +8,61 @@ function delay(time) {
     });
  }
 
-async function go() {
-    puppeteerExtra.use(stealthPlugin());
+puppeteerExtra.use(stealthPlugin());
 
-    const browser = await puppeteerExtra.launch({
-        headless: false,
-        // headless: "new",
-        // devtools: true,
-        PUPPETEER_SKIP_CHROMIUM_DOWNLOAD: true,
-        //Executable path for mac
-        executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-    });
-  
+let browser = await puppeteerExtra.launch({
+    headless: "new",
+    // headless: "new",
+    // devtools: true,
+    PUPPETEER_SKIP_CHROMIUM_DOWNLOAD: true,
+    //Executable path for mac
+    executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+});
+
+
+async function getSchools() {
     const page = await browser.newPage();
 
     try {
-        await page.goto(
-          "https://www.cmsk12.org/alexandergrahamMS"
-        );
+        await page.goto("https://www.cmsk12.org");
     } catch (error) {
         console.log("Error going to page");
     }
 
+    const allSchoolsBtn = "button#gb-school-trigger";
+    await page.waitForSelector(allSchoolsBtn);
+    await page.click(allSchoolsBtn);
 
+    await delay(2000);
 
+    await page.waitForSelector('li.cs-mega-school');
+    const schoolLis = await page.$$('li.cs-mega-school');
 
+    for(let school of schoolLis){
+        const name = await school.$eval('a', node => node.getAttribute('href'));
+        if(name === "/elementary-schools") continue;
+
+        try {
+            await go(name, `https://www.cmsk12.org${name}`);
+        } catch (error) {
+            console.log("Page closed due to error, moving onto next");
+        }
+    }
+
+    page.close();
+}
+
+await getSchools();
+
+async function go(school, url) {
+
+    const page = await browser.newPage();
+
+    try {
+        await page.goto(url);
+    } catch (error) {
+        console.log("Error going to page");
+    }
 
 
     await page.waitForSelector('ul#channel-navigation')
@@ -40,20 +70,26 @@ async function go() {
     await link.hover();
 
 
-    const facultyLink = await page.$('ul.sw-channel-dropdown > li ul > li:nth-of-type(2) a');
-    await facultyLink.click();
+    await delay(2000);
 
+
+    const testAs = await page.$$('li > a');
+
+    for (let i = 0; i < testAs.length; i++) {
+        const test = testAs[i];
+        const hasSpan = await test.$('span');
+        if (hasSpan) {
+            const text = await test.$eval('span', span => span.textContent);
+            if (text === "Faculty & Staff"){
+                test.click();
+                break;
+            }
+        }
+    }
 
     const teacherSelector = 'li[data-name="Teachers"]';
     await page.waitForSelector(teacherSelector);
     await page.click(teacherSelector);
-
-
-
-
-
-
-
 
     await page.waitForSelector('ul.staff-directory-pagination-list');
     const paginationUl = await page.$('ul.staff-directory-pagination-list');
@@ -76,12 +112,20 @@ async function go() {
 
         for(let teacher of teachersUl){
             const name = await teacher.$eval('.staff-directory-name', node => node.textContent);
-            const title = await teacher.$eval('.staff-directory-detail', node => node.textContent);
-            const email = await teacher.$eval('a[href^="mailto"]', node => 
-                node.getAttribute('href').slice(7));
-            console.log(name, title, email);
+            const title = await teacher.$eval('.staff-directory-detail', node => String(node.textContent));
+            const email = await teacher.$eval('a[href^="mailto"]', node => node.getAttribute('href').slice(7));
+
+
+            console.log(school.split("/")[1])
+            fs.appendFile(`./data/${school.split("/")[1]}.csv`, `${name},${title.split(",")[1].trim()},${email}\n`, function (err) {
+                if (err) throw err;
+                console.log('Teacher Added!');
+            });
         }
     }
+
+    page.close();
+    console.log("Done")
 }
 
-go();
+//go('AG-Middle', 'https://www.cmsk12.org/alexandergrahamMS');
